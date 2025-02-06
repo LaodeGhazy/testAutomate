e5b82f6805b31025ca052b0a10ca7e7a527fd42c
Laode Ghazy <ghazylaode002@gmail.com>
2025-02-06 13:03:42 -0500

    Menambahkan file baru

---
+++ pipeline.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+    environment {
+        CONTAINER_NAME = "my-web-app" // Ganti dengan nama container Docker Anda yang sebenarnya
+    }
+    stages {
+        stage('Checkout Code') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'server_aplikasi_ssh', url: 'https://github.com/LaodeGhazy/project1.git']]]) // Kredensial Git di sini
+            }
+        }
+        stage('Periksa Status Aplikasi') {
+            steps {
+                sh './check_app_status.sh' // Pastikan path dan permission execute sudah benar
+            }
+            post {
+                failure {
+                    steps {
+                        sshagent(credentials: ['server_aplikasi_ssh']) { // Kredensial SSH di sini
+                            sh "docker restart ${my-web-app}" // Gunakan variabel dari environment
+                        }
+                        // Email notifikasi (opsional)
+                    }
+                }
+                success {
+                    steps {
+                        // Email notifikasi (opsional)
+                    }
+                }
+            }
+        }
+    }
+}
