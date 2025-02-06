9462297d00339084870d30534720b13fd98becdf
Laode Ghazy <ghazylaode002@gmail.com>
2025-02-06 14:24:11 -0500

    anjay

--- check_app_status.sh
+++ check_app_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 umask 022
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000)
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://172.17.0.3:5000)
 echo "Status Code: $STATUS_CODE" # Tambahkan baris ini
 if [[ $STATUS_CODE -eq 200 ]]; then
   echo "Aplikasi web berjalan."
