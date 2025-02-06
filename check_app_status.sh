b06d2a3d6b508034e442a7889b8b73f520e0e5b5
Laode Ghazy <ghazylaode002@gmail.com>
2025-02-06 13:26:56 -0500

    Set executable permission for check_app_status.sh

--- check_app_status.sh
+++ check_app_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-umask 022 
+umask 022
 STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000)
-
+echo "Status Code: $STATUS_CODE" # Tambahkan baris ini
 if [[ $STATUS_CODE -eq 200 ]]; then
   echo "Aplikasi web berjalan."
 else
